744d0535f79d86acdec5d80932a6b369775ea16e
Kevin <staffordkevinjames@gmail.com>
2023-09-14 15:31:07 -0400

    updates challenge.js

--- js/challenge.js
+++ js/challenge.js
@@ -3,6 +3,13 @@
 const minusOneFromCounter = document.querySelector(`button`) 
 
 minusOneFromCounter.addEventListener(`click`, (e)=>{
-    const lowerCounter = document.getElementById(`minus`) 
+    const lowerCounter = document.getElementById(`.counter`) 
     lowerCounter.innerText-- 
 }) 
+
+const plusOneToCounter = document.querySelector('.counter')
+
+plusOneToCounter.addEventListener('click', (e) => {
+    const plusCounter = document.getElementById('plus')
+    plusCounter.innerText++
+})
