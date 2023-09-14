9664ab1522adf4fabf180fa81fc9b81821c1d438
Austin Frick <austin.frick429@gmail.com>
2023-09-14 14:52:30 -0400

    adds to challange.js

---
+++ js/challenge.js
@@ -0,0 +1,8 @@
+// const minusButton = document.getElementById(`minus`) 
+
+const minusOneFromCounter = document.querySelector(`button`) 
+
+minusOneFromCounter.addEventListener(`click`, (e)=>{
+    const lowerCounter = document.getElementById(`minus`) 
+    lowerCounter.innerText-- 
+}) 
