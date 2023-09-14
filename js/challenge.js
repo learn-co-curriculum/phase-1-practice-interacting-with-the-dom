0e19e4ff26345615ed6dd2d94a5f61e647f8b67f
Kevin <staffordkevinjames@gmail.com>
2023-09-14 16:30:21 -0400

    starts likes function git push origin domlistening

--- js/challenge.js
+++ js/challenge.js
@@ -1,15 +1,23 @@
-// const minusButton = document.getElementById(`minus`) 
 
 const minusOneFromCounter = document.querySelector(`button`) 
 
+const updateCounter = document.getElementById(`counter`)
+
 minusOneFromCounter.addEventListener(`click`, (e)=>{
-    const lowerCounter = document.getElementById(`.counter`) 
-    lowerCounter.innerText-- 
+    updateCounter.innerText-- 
 }) 
 
-const plusOneToCounter = document.querySelector('.counter')
+const plusOneToCounter = document.getElementById('plus')
 
 plusOneToCounter.addEventListener('click', (e) => {
-    const plusCounter = document.getElementById('plus')
-    plusCounter.innerText++
+    updateCounter.innerText++
 })
+
+const addNumberLikes = document.getElementById('heart')
+
+addNumberLikes.addEventListener('click', (e) => {
+    /* add text "this number has been liked this many times" 
+    to the unordered list section */
+    const likeMessages = document.getElementsByClassName('likes')
+    likeMessages.addEventListener
+} )
